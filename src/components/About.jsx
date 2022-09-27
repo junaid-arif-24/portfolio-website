006a89d0d94625da68fd98c377c811539cee5026
junaid-arif-24 <mohdjunaid7078@gmail.com>
2022-09-27 16:35:48 +0530

    minor commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div
       name="about"
-      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white py-48">
+      className="w-full h-full bg-gradient-to-b from-gray-800 to-black text-white pt-40">
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -9,14 +9,20 @@ const Portfolio = () => {
     {
       id: 1,
       src: codeEditor,
+      demo:1,
+      code:"https://github.com/junaid-arif-24/Code-editor.git"
     },
     {
       id: 2,
       src: portfolioWeb,
+      demo:"",
+      code:"https://github.com/junaid-arif-24/Code-editor.git"
     },
     {
       id: 3,
       src: gymReg,
+      demo:"",
+      code:"https://github.com/junaid-arif-24/Code-editor.git"
     },
    
   ];
@@ -24,7 +30,7 @@ const Portfolio = () => {
   return (
     <div
       name="portfolio"
-      className="bg-gradient-to-b from-black to-gray-800 w-full text-white h-full pb-30" 
+      className="bg-gradient-to-b from-black to-gray-800 w-full text-white h-full pt-32 pb-30" 
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full ">
         <div className="pb-8">
@@ -35,7 +41,7 @@ const Portfolio = () => {
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          {portfolios.map(({ id, src }) => (
+          {portfolios.map(({ id, src ,code}) => (
             <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
               <img
                 src={src}
@@ -44,10 +50,10 @@ const Portfolio = () => {
               />
               <div className="flex items-center justify-center">
                 <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                 <a href ="https://github.com/junaid-arif-24/Code-editor.git">Demo</a> 
+                 <a href ={code}>Demo</a> 
                 </button>
                 <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                  Code
+                <a href ={code}>Code</a> 
                 </button>
               </div>
             </div>
